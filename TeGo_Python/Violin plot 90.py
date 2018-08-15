ada9f4e3901a761f5cabe8c66d81e603555bbe5a
davidpofo <dampofo@umd.edu>
2018-08-15 13:34:53 -0400

    Add files via upload
    Trying some stuff

--- TeGo_Python/Violin plot 90.py
+++ TeGo_Python/Violin plot 90.py
@@ -1,11 +1,119 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.collections
 import seaborn as sns
+import pandas as pd
+
+def offset_violinplot_halves(ax, delta, width, inner, direction):
+    """
+    This function offsets the halves of a violinplot to compare tails
+    or to plot something else in between them. This is specifically designed
+    for violinplots by Seaborn that use the option `split=True`.
+
+    For lines, this works on the assumption that Seaborn plots everything with
+     integers as the center.
+
+    Args:
+     <ax>    The axis that contains the violinplots.
+     <delta> The amount of space to put between the two halves of the violinplot
+     <width> The total width of the violinplot, as passed to sns.violinplot()
+     <inner> The type of inner in the seaborn
+     <direction> Orientation of violinplot. 'hotizontal' or 'vertical'.
+
+    Returns:
+     - NA, modifies the <ax> directly
+    """
+    # offset stuff
+    if inner == 'sticks':
+        lines = ax.get_lines()
+        for line in lines:
+            if direction == 'horizontal':
+                data = line.get_ydata()
+                print(data)
+                if int(data[0] + 1)/int(data[1] + 1) < 1:
+                    # type is top, move neg, direction backwards for horizontal
+                    data -= delta
+                else:
+                    # type is bottom, move pos, direction backward for hori
+                    data += delta
+                line.set_ydata(data)
+            elif direction == 'vertical':
+                data = line.get_xdata()
+                print(data)
+                if int(data[0] + 1)/int(data[1] + 1) < 1:
+                    # type is left, move neg
+                    data -= delta
+                else:
+                    # type is left, move pos
+                    data += delta
+                line.set_xdata(data)
+
+
+    for ii, item in enumerate(ax.collections):
+        # axis contains PolyCollections and PathCollections
+        if isinstance(item, matplotlib.collections.PolyCollection):
+            # get path
+            path, = item.get_paths()
+            vertices = path.vertices
+            half_type = _wedge_dir(vertices, direction)
+            # shift x-coordinates of path
+            if half_type in ['top','bottom']:
+               if inner in ["sticks", None]:
+                    if half_type == 'top': # -> up
+                        vertices[:,1] -= delta
+                    elif half_type == 'bottom': # -> down
+                        vertices[:,1] += delta
+            elif half_type in ['left', 'right']:
+                if inner in ["sticks", None]:
+                    if half_type == 'left': # -> left
+                        vertices[:,0] -= delta
+                    elif half_type == 'right': # -> down
+                        vertices[:,0] += delta
+
+def _wedge_dir(vertices, direction):
+    """
+    Args:
+      <vertices>  The vertices from matplotlib.collections.PolyCollection
+      <direction> Direction must be 'horizontal' or 'vertical' according to how
+                   your plot is laid out.
+    Returns:
+      - a string in ['top', 'bottom', 'left', 'right'] that determines where the
+         half of the violinplot is relative to the center.
+    """
+    if direction == 'horizontal':
+        result = (direction, len(set(vertices[1:5,1])) == 1)
+    elif direction == 'vertical':
+        result = (direction, len(set(vertices[-3:-1,0])) == 1)
+    outcome_key = {('horizontal', True): 'bottom',
+                   ('horizontal', False): 'top',
+                   ('vertical', True): 'left',
+                   ('vertical', False): 'right'}
+    # if the first couple x/y values after the start are the same, it
+    #  is the input direction. If not, it is the opposite
+    return outcome_key[result]
 
 sns.set()
+# initialise new axes;
+# if there is random other crap on the axis (e.g. a previous plot),
+# the hacky code below won't work
+fig, (ax1, ax2) = plt.subplots(2)
 
-# Use cubehelix to get a custom sequential palette
-pal = sns.cubehelix_palette(p, rot=-.5, dark=.3)
 
-# Show each distribution with both violins and points
-sns.violinplot(data=SQL_df, palette=pal, inner="points")
-title(main = "Average clicks for new vs invested users by COI", ylab="Clicks", xlab="COI")
+inner = "sticks" # Note: 'box' is default
+width = 0.75
+delta = 0.05
+final_width = width - delta
+
+sns.violinplot(data=SQL_df, x='y', y='x',
+               split=True, hue = 'z',
+               ax = ax1, inner='sticks',
+               bw = 0.2)
+sns.violinplot(data=SQL_df, x='x', y='y',
+               split=True, hue = 'z',
+               ax = ax2, inner='sticks',
+               bw = 0.2)
+
+offset_violinplot_halves(ax1, delta, final_width, inner, 'horizontal')
+offset_violinplot_halves(ax2, delta, final_width, inner, 'vertical')
+
+plt.show()
