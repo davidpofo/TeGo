1996d6997ac98599fcb4cea8e3791b403a934d1a
davidpofo <dampofo@umd.edu>
2018-09-06 13:53:27 -0400

    Add files via upload
    Updates for Time series model

--- TeGo_Python/click_ts.py
+++ TeGo_Python/click_ts.py
@@ -14,7 +14,13 @@ import matplotlib.pylab as plt
 #and %d    Day of the month as a zero-padded decimal number.
 dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m-%d')
 data = pd.read_csv('afg_clicks.csv', parse_dates=['Date'], index_col='Date',date_parser=dateparse)
+
+# Removes Nans and infs
+#data = pd.DataFrame(data.replace([np.inf, -np.inf], np.nan))
+#data = data.fillna(method='ffill')#Forward fills the ts_log
+#data = data.fillna(method='bfill')
 print(data.head())
+
 # Checking the type of the index
 data.index # dtype=’datetime[ns]’ which confirms that it is a datetime object.
 from datetime import datetime
@@ -46,7 +52,7 @@ def test_stationarity(timeseries):
     print(dfoutput)
 
 test_stationarity(ts)
-# Estimating & Eliminating Trend
+# Estimating & Eliminating Trend'
 ts_log = np.log(ts)
 plt.plot(ts_log)
 
@@ -74,5 +80,93 @@ ts_log_diff.dropna(inplace=True)
 test_stationarity(ts_log_diff)
 
 #Need to add the frequency for Docomposition
-ts_log= ts_log.asfreq('D')
+ts_log = ts_log.asfreq('D')
+ts_log.index
+ts_log.dropna(inplace=True)
 
+# Decomposing
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+#Need to set Frequency and indicate I want the values to be decomposed
+decomposition = seasonal_decompose(ts_log.values, freq = 1)
+
+trend = decomposition.trend
+seasonal = decomposition.seasonal
+residual = decomposition.resid
+
+plt.subplot(411)
+plt.plot(ts_log, label='Original')
+plt.legend(loc='best')
+plt.subplot(412)
+plt.plot(trend, label='Trend')
+plt.legend(loc='best')
+plt.subplot(413)
+plt.plot(seasonal,label='Seasonality')
+plt.legend(loc='best')
+plt.subplot(414)
+plt.plot(residual, label='Residuals')
+plt.legend(loc='best')
+plt.tight_layout()
+
+# Stationary Residauls
+ts_log_decompose = residual
+ts_log_decompose.dropna(inplace=True)
+test_stationarity(ts_log_decompose)
+
+# Forecasting a Time Series
+#ACF and PACF plots:
+from statsmodels.tsa.stattools import acf, pacf
+lag_acf = acf(ts_log_diff, nlags=20)
+lag_pacf = pacf(ts_log_diff, nlags=20, method='ols')
+#Plot ACF:
+plt.subplot(121)
+plt.plot(lag_acf)
+plt.axhline(y=0,linestyle='--',color='gray')
+plt.axhline(y=-1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.axhline(y=1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.title('Autocorrelation Function')
+
+#Plot PACF
+plt.subplot(122)
+plt.plot(lag_pacf)
+plt.axhline(y=0,linestyle='--',color='gray')
+plt.axhline(y=-1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.axhline(y=1.96/np.sqrt(len(ts_log_diff)),linestyle='--',color='gray')
+plt.title('Partial Autocorrelation Function')
+plt.tight_layout()
+# AR Model
+from statsmodels.tsa.arima_model import ARIMA
+model = ARIMA(ts_log, order=(2, 1, 0))
+results_AR = model.fit(disp=-1)
+plt.plot(ts_log_diff)
+plt.plot(results_AR.fittedvalues, color='red')
+plt.title('RSS: %.4f'% sum((results_AR.fittedvalues-ts_log_diff)**2))
+# MA model
+model = ARIMA(ts_log, order=(0, 1, 2))
+results_MA = model.fit(disp=-1)
+plt.plot(ts_log_diff)
+plt.plot(results_MA.fittedvalues, color='red')
+plt.title('RSS: %.4f'% sum((results_MA.fittedvalues-ts_log_diff)**2))
+# Combined Model
+model = ARIMA(ts_log, order=(2, 1, 2))
+results_ARIMA = model.fit(disp=-1)
+plt.plot(ts_log_diff)
+plt.plot(results_ARIMA.fittedvalues, color='red')
+plt.title('RSS: %.4f'% sum((results_ARIMA.fittedvalues-ts_log_diff)**2))
+# Back to Original Scale
+predictions_ARIMA_diff = pd.Series(results_ARIMA.fittedvalues, copy=True)
+print(predictions_ARIMA_diff.head())
+
+
+predictions_ARIMA_diff_cumsum = predictions_ARIMA_diff.cumsum()
+print(predictions_ARIMA_diff_cumsum.head())
+
+
+predictions_ARIMA_log = pd.Series(ts_log.ix[0], index=ts_log.index)
+predictions_ARIMA_log = predictions_ARIMA_log.add(predictions_ARIMA_diff_cumsum,fill_value=0)
+predictions_ARIMA_log.head()
+
+predictions_ARIMA = np.exp(predictions_ARIMA_log)
+plt.plot(ts)
+plt.plot(predictions_ARIMA)
+plt.title('RMSE: %.4f'% np.sqrt(sum((predictions_ARIMA-ts)**2)/len(ts)))
